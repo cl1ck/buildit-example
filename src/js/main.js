92f4fb98e7a7ca3c6f9d2694478d58393b305cc9
Michel Hofmann <3rdgeist@gmail.com>
2015-05-13 17:41:25 +0200

    added cs error

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 import Person from './person'
 
-let person = new Person('test')
-let greeter = document.getElementById('greeter')
+let person = new Person('test'), greeter = document.getElementById('greeter')
 let message = 'Hello ' + person.name
 
 greeter.innerHTML = message
